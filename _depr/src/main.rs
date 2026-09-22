c62bcec3c16d3e2fb38dd0cdf800c742052c05cb
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-09-22 15:44:29 +0100

    refactor(_depr): remove deprecated modules and prune Cargo.lock dependencies

---
+++ _depr/src/main.rs
@@ -0,0 +1,12 @@
+mod ai;
+mod cli;
+mod click;
+mod locales;
+mod parser;
+mod prompt;
+mod screenshot;
+mod to_base64;
+
+fn main() {
+  cli::start();
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-mod ai;
-mod cli;
-mod click;
-mod locales;
-mod parser;
-mod prompt;
-mod screenshot;
-mod to_base64;
-
 fn main() {
-  cli::start();
+	println!("Hello Lenix!");
 }
