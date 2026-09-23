6688910adafad4cca5b95740e4315951e591d1fb
lenixdev <149199196+LenixDev@users.noreply.github.com>
2026-09-23 12:48:20 +0100

    chore(project): add Xcode project files for macOS build

---
+++ macos/sysenix/sysenix/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sysenix
+//
+//  Created by Lenix on 23/9/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ macos/sysenix/sysenix/sysenixApp.swift
@@ -0,0 +1,17 @@
+//
+//  sysenixApp.swift
+//  sysenix
+//
+//  Created by Lenix on 23/9/2026.
+//
+
+import SwiftUI
+
+@main
+struct sysenixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
